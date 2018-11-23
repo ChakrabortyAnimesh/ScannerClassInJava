0105a57c80c5636071f4a2bacab1b5e3294f79df
Animesh <animesh.b.tech12345@gmail.com>
2018-11-22 21:31:26 -1200

    Initial commit

---
+++ src/com/company/java/Main.java
@@ -0,0 +1,15 @@
+package com.company.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String st=sc.next();
+        System.out.println("I know, Your name is: "+st);
+
+    }
+}
